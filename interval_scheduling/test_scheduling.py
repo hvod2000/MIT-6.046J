7e431f36848079e493a38400304f90b2eac5f20c
Khamkou Uladzislau <hamkov@vivaldi.net>
2021-01-05 00:20:20 +0300

    interval-scheduling: fix test_scheduling.py
    test_scheduling was overly strict, it accepted only one right answer.
    Now it accepts all right answers.

--- interval_scheduling/test_scheduling.py
+++ interval_scheduling/test_scheduling.py
@@ -6,14 +6,15 @@ class TestScheduling(unittest.TestCase):
     def test(self):
         expectation = [
             ({(0, 1)}, [(0, 1)]),
-            ({(0, 2), (1, 2)}, [(0, 2)]),
+            ({(0, 2), (1, 2)}, ([(0, 2)], [(1, 2)])),
             ({(1, 5), (0, 2), (3, 6)}, [(0, 2), (3, 6)]),
             ({(5, 100), (2, 7), (8, 12), (15, 80)}, [(2, 7), (8, 12), (15, 80)]),
-            ({(0, 2), (1, 5)}, [(1, 5)]),
+            ({(0, 2), (1, 5)}, ([(1, 5)], [(0, 2)])),
         ]
         for argument, expected_output in expectation:
             actual_output = scheduling.get_schedule(argument)
-            self.assertEqual(
+            test = self.assertIn if isinstance(expected_output, tuple) else self.assertEqual
+            test(
                 actual_output, expected_output, f"Input: {argument}"
             )
 
