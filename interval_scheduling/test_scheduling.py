a3c2427866842f88b2b7438c168a38c0c8c69f4b
Khamkou Uladzislau <hamkov@vivaldi.net>
2021-01-04 23:53:55 +0300

    interval-scheduling: add more tests

--- interval_scheduling/test_scheduling.py
+++ interval_scheduling/test_scheduling.py
@@ -6,10 +6,16 @@ class TestScheduling(unittest.TestCase):
     def test(self):
         expectation = [
             ({(0, 1)}, [(0, 1)]),
+            ({(0, 2), (1, 2)}, [(0, 2)]),
+            ({(1, 5), (0, 2), (3, 6)}, [(0, 2), (3, 6)]),
+            ({(5, 100), (2, 7), (8, 12), (15, 80)}, [(2, 7), (8, 12), (15, 80)]),
+            ({(0, 2), (1, 5)}, [(1, 5)]),
         ]
         for argument, expected_output in expectation:
             actual_output = scheduling.get_schedule(argument)
-            self.assertEqual(actual_output, expected_output)
+            self.assertEqual(
+                actual_output, expected_output, f"Input: {argument}"
+            )
 
 
 if __name__ == "__main__":
