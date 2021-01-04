a09dc2118764ee22270036b6e139150b18b93163
Khamkou Uladzislau <hamkov@vivaldi.net>
2021-01-05 00:12:36 +0300

    interval-scheduling: implement get_schedule()

--- interval_scheduling/scheduling.py
+++ interval_scheduling/scheduling.py
@@ -1,2 +1,8 @@
-def get_schedule(_):
-    return [(0, 1)]
+def get_schedule(intervals):
+    result = []
+    finish = float("-inf")
+    for interval in sorted(intervals, key=lambda i: i[1]):
+        if finish <= interval[0]:
+            finish = interval[1]
+            result.append(interval)
+    return result
