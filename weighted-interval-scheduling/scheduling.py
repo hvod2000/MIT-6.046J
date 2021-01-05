1d0bd3ce8dcb687c8bfab0febe64b08c1e744e34
Khamkou Uladzislau <hamkov@vivaldi.net>
2021-01-05 23:35:12 +0300

    weighted-interval-scheduling: implementation

--- weighted-interval-scheduling/scheduling.py
+++ weighted-interval-scheduling/scheduling.py
@@ -1,8 +1,26 @@
 def get_schedule(intervals):
-    result = []
-    finish = float("-inf")
-    for interval in sorted(intervals, key=lambda i: i[1]):
-        if finish <= interval[0]:
-            finish = interval[1]
-            result.append(interval)
-    return result
+    intervals = list(sorted(intervals, key=lambda i: -i[1]))
+    intervals.append((None, float('-inf')))
+    p = [-1 for i in range(len(intervals))]
+    for i, interval in enumerate(intervals):
+        for j in range(i - 1, -1, -1):
+            pint = intervals[j]
+            if pint[0] > interval[1]:
+                p[i] = j
+                break
+    optR = [0 for i in range(len(intervals))]
+    take = [None for i in range(len(intervals))]
+    for i in range(len(intervals)):
+        v_if_leave = optR[i - 1]
+        v_if_take = optR[p[i]]
+        if p[i] != -1:
+            v_if_take += intervals[p[i]][2]
+        take[i] = v_if_take > v_if_leave
+        optR[i] = v_if_take if take[i] else v_if_leave
+    def get_result():
+        tail = len(intervals) - 1
+        while tail > 0:
+            if take[tail]:
+                yield intervals[p[tail]]
+            tail = p[tail] if take[tail] else tail - 1
+    return list(get_result())
