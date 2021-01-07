fd14dd9fa54acf50be560c0b92c61f5fb27676c1
Khamkou Uladzislau <hamkov@vivaldi.net>
2021-01-07 12:46:11 +0300

    weighted-interval-scheduling: refactoring
    get_schedule() was using unnatural intervals order.
    Now it sorts them by start time

--- weighted-interval-scheduling/scheduling.py
+++ weighted-interval-scheduling/scheduling.py
@@ -1,26 +1,22 @@
 def get_schedule(intervals):
-    intervals = list(sorted(intervals, key=lambda i: -i[1]))
-    intervals.append((None, float('-inf')))
-    p = [-1 for i in range(len(intervals))]
-    for i, interval in enumerate(intervals):
-        for j in range(i - 1, -1, -1):
-            pint = intervals[j]
-            if pint[0] > interval[1]:
+    intervals = list(sorted(intervals, key=lambda i: i[0])) + [[float('inf')]]
+    p = [len(intervals) - 1 for i in intervals]
+    for i in range(len(intervals) - 2, -1, -1):
+        for j in range(i + 1, len(intervals)):
+            if intervals[j][0] >= intervals[i][1]:
                 p[i] = j
                 break
     optR = [0 for i in range(len(intervals))]
     take = [None for i in range(len(intervals))]
-    for i in range(len(intervals)):
-        v_if_leave = optR[i - 1]
-        v_if_take = optR[p[i]]
-        if p[i] != -1:
-            v_if_take += intervals[p[i]][2]
+    for i in range(len(intervals) - 2, -1, -1):
+        v_if_leave = optR[i + 1]
+        v_if_take = optR[p[i]] + intervals[i][2]
         take[i] = v_if_take > v_if_leave
         optR[i] = v_if_take if take[i] else v_if_leave
     def get_result():
-        tail = len(intervals) - 1
-        while tail > 0:
+        tail = 0
+        while tail < len(intervals) - 1:
             if take[tail]:
-                yield intervals[p[tail]]
-            tail = p[tail] if take[tail] else tail - 1
+                yield intervals[tail]
+            tail = p[tail] if take[tail] else tail + 1
     return list(get_result())
