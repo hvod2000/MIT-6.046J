d96c6e04c765fcd22bec6da009f03a188d4fecd5
Khamkou Uladzislau <hamkov@vivaldi.net>
2021-01-07 21:45:25 +0300

    median-finding: add small optimizations

--- median-finding/finding.py
+++ median-finding/finding.py
@@ -3,15 +3,15 @@ def get_median_of_columns(elements):
     for i, elem in enumerate(elements):
         cols[i // 5].append(elem)
     medians = [col[(len(col) - 1) // 2] for col in cols]
-    if len(medians) == 1:
-        return medians[0]
     return get_by_index(medians, (len(medians) - 1) // 2)
 
 def get_by_index(elements, i):
+    if len(elements) == 1:
+        return elements[0]
     x = get_median_of_columns(elements)
     elements_before = [e for e in elements if e < x]
     elements_after = [e for e in elements if e > x]
-    rank_of_x = len([e for e in elements if e <= x])
+    rank_of_x = len(elements) - len(elements_after)
     if len(elements_before) <= i < rank_of_x:
         return x
     elif rank_of_x > i:
