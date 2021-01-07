3b0d3ccf766ac82dd14d33bbfed6429e14c6724b
Khamkou Uladzislau <hamkov@vivaldi.net>
2021-01-07 20:44:30 +0300

    median-finding: add test for finding by index

--- median-finding/test_finding.py
+++ median-finding/test_finding.py
@@ -10,6 +10,15 @@ class TestScheduling(unittest.TestCase):
             actual_output = get_by_index(arg, (len(arg) - 1) // 2)
             self.assertEqual(actual_output, expected_output, f"In: {arg}")
 
+    def test_index(self):
+        for _ in range(16):
+            elements = [randint(0, 2**10) for _ in range(2**randint(1, 11))]
+            index = randint(0, len(elements) - 1)
+            expected_output = list(sorted(elements))[index]
+            actual_output = get_by_index(elements, index)
+            self.assertEqual(actual_output, expected_output,
+                    f"In: {index}, {elements}")
+
 
 if __name__ == "__main__":
     unittest.main()
